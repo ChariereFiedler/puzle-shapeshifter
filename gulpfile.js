2c38b23717374d8cd290b267773f937ea38fbf48
Cedric Chariere Fiedler <chariere.fiedler.cedric@gmail.com>
2017-02-10 01:29:37 +0100

    Refactorization Handlebars Compilation: externalize into own gulp task file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const gulp = require('gulp');
-const markdown = require('gulp-markdown');
-const del = require('del');
-const handlebars = require('handlebars');
-const frontMatter = require('gulp-front-matter');
-const tap = require('gulp-tap');
 
 gulp.task('clean', () => {
     return del([
@@ -36,43 +31,20 @@ require('./gulp_tasks/typescript')(gulp, configTest, "Test");
 /*---------------------------------------------*/
 /* Hbs Compilation */
 /*---------------------------------------------*/
-gulp.task('compile', () => {
-    let templates = {};
-    return gulp.src('_toCompile/*.md')
-        .pipe(frontMatter({
-            property: 'data',
-            remove: true
-        }))
-        .pipe(markdown())
-        .pipe(tap(function (file) {
 
-            let template = file.data.template;
-            let compile = (template) => {
-                return template({
-                    data : file.data,
-                    content: file.contents.toString()
-                });
-            };
-            if(template !== undefined) {
-                if(templates[template] === undefined) {
-                    let filepath = path.join('./_templates', template + '.hbs');
-                    console.log(filepath);
-                    let data = fs.readFileSync(filepath);
+const hbsCompileParams = {
+    taskName: 'compile',
+    src: '_toCompile/*.md',
+    dest: '_compiled',
+    templateLocation: './_templates'
+};
 
-                    templates[template] = handlebars.compile(data.toString());
-
-                }
-                file.contents = new Buffer(compile(templates[template]), "utf-8")
-            }
-
-        }))
-        .pipe(gulp.dest('_compiled'));
-});
+require('./gulp_tasks/handlebars-compilation')(gulp, hbsCompileParams);
 
 /*---------------------------------------------*/
 gulp.task('watch', ()=> {
-    gulp.watch(path.join(config.srcDirectory, './src/**/*.ts'), ['typescript']);
-    gulp.watch(path.join(configTest.srcDirectory, './test/**/*.ts'), ['typescriptTest']);
+    gulp.watch(path.join(config.srcDirectory, './**/*.ts'), ['typescript']);
+    gulp.watch(path.join(configTest.srcDirectory, './**/*.ts'), ['typescriptTest']);
 });
 
 gulp.task('build', ['typescript', 'typescriptTest']);
--- test/build.ts
+++ test/build.ts
@@ -10,9 +10,9 @@ import * as fs from "fs";
 
 describe("Gulp build", () => {
     it("test.md with page.hbs to test.html", ()=> {
-        execSync("gulp build");
+        execSync("gulp compile");
         let result = fs.readFileSync("_compiled/test.html", "utf8");
-        let norm = fs.readFileSync("test/dataset/build.html", "utf8");
+        let norm = fs.readFileSync("test/dataset/test.html", "utf8");
         expect(result).to.equal(norm);
     });
 });
